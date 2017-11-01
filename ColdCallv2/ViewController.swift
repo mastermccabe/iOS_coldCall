46c7baa1c6db127f5bcdcc4d40cfab13fef53865
McCabe <mccabe@wambl.com>
2017-11-01 14:09:24 -0700

    working

--- ColdCallv2/ViewController.swift
+++ ColdCallv2/ViewController.swift
@@ -9,9 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    var currentName = "Ready?"
+    var randVal : Int = 0
+    var rand2 : Int = 0
+    var colors: String = "black"
+    
+    
+    @IBAction func callButtonPressed(_ sender: UIButton) {
+        
+        randVal = Int(arc4random_uniform(UInt32(nameBank.count-1))+1)
+        rand2 = Int(arc4random_uniform(UInt32(5))+1)
+        if (rand2 <= 2){
+            numberLabel.textColor = UIColor.red
+        }
+        else if (rand2 <= 4){
+                numberLabel.textColor = UIColor.orange
+            }
+        
+        else {
+         numberLabel.textColor = UIColor.green
+        }
 
+        updateUI()
+    }
+   
+    let nameBank = ["Ready?","Troy","Jake","Brad","Chaz","Chris","Frat guy"]
+    
+    
+    func updateUI() {
+        nameLabel.text = nameBank[randVal]
+        if (rand2 > 0){
+        numberLabel.text = String(rand2)
+        }
+//        questionLabel.text = quizBank[currentIndex].0
+//        answerLabel.text = quizBank[currentIndex].1
+//        answerLabel.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+          updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
